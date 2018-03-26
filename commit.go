0acfd287c9a34268a035c6460ae18b51efd5295e
unknown <benjamin.coleman@me.com>
2018-03-26 14:12:45 +0100

    removed carriage return, and runs an interpreter explicitly for windows

--- commit.go
+++ commit.go
@@ -254,7 +254,7 @@ func RunReservePlugin(task Task) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("error marshalling task: %v\n", err)
 	}
-	cmd := exec.Command(".git/gitdo/plugins/reserve_"+config.Plugin, string(bTask))
+	cmd := exec.Command("python", ".git/gitdo/plugins/reserve_"+config.Plugin, string(bTask))
 	res, err := cmd.CombinedOutput()
 	if err != nil {
 		return "", fmt.Errorf("error running plugin: %v\n", string(res))
