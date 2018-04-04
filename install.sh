db31c15f488a5f41630a86676a74eccd3f639d12
Ben Coleman <benjamin.coleman@me.com>
2018-04-04 17:32:09 +0100

    notify user when installing

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 DIR=$HOME/.gitdo
 mkdir $DIR
+echo "Copying plugins..."
+echo "Copying hooks..."
 cp -r ./plugins $DIR
 cp -r ./hooks $DIR
 
 GOARCH=`uname -p`
 GOOS=`uname`
 VERSIONTOCP="gitdo_${GOOS}_${GOARCH}"
+echo "Copying $VERSIONTOCP to your /usr/local/bin/ ..."
 cp $VERSIONTOCP /usr/local/bin/gitdo
+echo "Done."
