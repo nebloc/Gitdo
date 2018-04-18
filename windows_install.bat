c33dddb19e7c5f07a07218e768d196a82c483f38
unknown <benjamin.coleman@me.com>
2018-04-18 14:29:10 +0100

    fixed executable not matching name

--- windows_install.bat
+++ windows_install.bat
@@ -3,5 +3,12 @@ MKDIR %APPDATA%\gitdo\plugins
 XCOPY /s/e/h/k/y %0\..\plugins %APPDATA%\gitdo\plugins
 MKDIR %APPDATA%\gitdo\hooks
 XCOPY /s/e/h/k/y %0\..\hooks %APPDATA%\gitdo\hooks
-COPY %0\..\gitdo_win_amd64.exe %windir%\System32\gitdo.exe
+
+@echo off
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+
+echo %OS%
+@echo on
+if %OS%==32BIT COPY %0\..\gitdo_win_32.exe %windir%\System32\gitdo.exe
+if %OS%==64BIT COPY %0\..\gitdo_win_64.exe %windir%\System32\gitdo.exe
 PAUSE
