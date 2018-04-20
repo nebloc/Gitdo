6ff746e69c91175122840b0949d74e6eb1a08b6d
Ben Coleman <benjamin.coleman@me.com>
2018-04-20 12:25:56 +0100

    removed a lot of the commit tests that are now controlled by versioncontrol package

--- app/commit_test.go
+++ app/commit_test.go
@@ -1,78 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"io/ioutil"
 	"os"
-	"os/exec"
-	"strings"
 	"testing"
 
 	"github.com/nebloc/gitdo/app/diffparse"
-	"github.com/nebloc/gitdo/app/versioncontrol"
 )
-
-func TestRegexs(t *testing.T) {
-	testStrings := []string{
-		"//TODO: Test that taggedIds with 13 works <22031810224912>",
-		"//TODO: Test that taggedIds with 14 works <125861826182573>",
-		"//TODO: Test that taggedIds with email works <benjamin.coleman@me.com:14861826182573>",
-	}
-	for _, str := range testStrings {
-		m := taggedReg.MatchString(str)
-		if !m {
-			t.Errorf("Expected match for tagged line: %s", str)
-		}
-	}
-}
-
-func TestCommit(t *testing.T) {
-	ctx, closeDir := setupForTest(t)
-	defer closeDir()
-
-	err := Commit(ctx)
-	if err != versioncontrol.ErrNotVCDir {
-		t.Errorf("Expected: %v, got: %v", versioncontrol.ErrNotVCDir, err)
-	}
-
-	testStartRepoHelper(t)
-
-	err = Commit(ctx)
-	if err != nil {
-		t.Errorf("Expected commit to return with no error, got: %v", err)
-	}
-
-	fileName := testMockFileHelper(t)
-
-	err = Commit(ctx)
-	if err != nil {
-		t.Errorf("didn't expect an error: %v", err)
-	}
-
-	bMock, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		t.Fatalf("couldn't read in file to against golden: %v", err)
-	}
-	if bytes.Compare([]byte(goldenFileContent), bMock) != 0 {
-		t.Errorf("expected:\n%s \n\ngot:\n%s", goldenFileContent, bMock)
-	}
-	tasks := testTaskFileCorrectHelper(t)
-	if len(tasks.NewTasks) != 1 {
-		t.Errorf("Should have 1 task in the new task area. Have: %d", len(tasks.NewTasks))
-	}
-
-	testCommitHelper(t)
-	testDeleteTaskCommentHelper(t, fileName)
-
-	err = Commit(ctx)
-	if err != nil {
-		t.Fatalf("unexpected: %v", err)
-	}
-	tasks = testTaskFileCorrectHelper(t)
-	if len(tasks.DoneTasks) != 1 {
-		t.Errorf("Should have 1 task in the done task area. Have: %d", len(tasks.DoneTasks))
-	}
-}
 
 func TestCheckTagged(t *testing.T) {
 	line := diffparse.SourceLine{
@@ -141,131 +75,35 @@ func TestCheckTaskRegex(t *testing.T) {
 	}
 }
 
-func TestGetDiffFromCmd(t *testing.T) {
-	_, closeDir := setupForTest(t)
-	defer closeDir()
+var origFile = []byte("1\n2\n3\n4\n5\n6\n7\n8\n9\n10")
+var newFile = []byte("1\n2\n3\n4\n5\n6\n7 <1234>\n8\n9\n10")
 
-	_, err := config.vc.GetDiff()
-	if err != versioncontrol.ErrNotVCDir {
-		t.Errorf("Expected not a git repo, got: %v", err)
-	}
-	testStartRepoHelper(t)
-	diff, err := config.vc.GetDiff()
-	if err != versioncontrol.ErrNoDiff {
-		t.Errorf("expected diff to be empty: %v: %v", err, diff)
-	}
-	file := testMockFileHelper(t)
-	testAddToGitHelper(t, file)
-	diff, err = config.vc.GetDiff()
+func TestMarkSourceLines(t *testing.T) {
+	fileName := "test.txt"
+	setupForTest(t)
+	err := ioutil.WriteFile(fileName, origFile, os.ModePerm)
 	if err != nil {
-		t.Errorf("Unexpected error getting diff: %v", err)
+		t.Fatal("Could not create test file")
 	}
-	if diff != mockDiffExample {
-		t.Errorf("Expected diff to be \n%v\nGot: \n%v", mockDiffExample, diff)
+	task := Task{
+		id:       "1234",
+		FileName: fileName,
+		TaskName: "7",
+		FileLine: 7,
+		Author:   "example@email.com",
+		Hash:     "",
+		Branch:   "",
 	}
-}
-
-const mockFileContent string = `package main
-
-import "fmt"
-
-// TODO: Test
-func main(){
-	fmt.Println("Hello Ben")
-}`
-
-const goldenFileContent string = `package main
-
-import "fmt"
-
-// TODO: Test <1234>
-func main(){
-	fmt.Println("Hello Ben")
-}`
-
-const mockDiffExample string = `diff --git a/main.go b/main.go
-new file mode 100755
-index 0000000..a30278c
---- /dev/null
-+++ b/main.go
-@@ -0,0 +1,8 @@
-+package main
-+
-+import "fmt"
-+
-+// TODO: Test
-+func main(){
-+	fmt.Println("Hello Ben")
-+}
-\ No newline at end of file`
-
-//////////////////////////////
-// Helper functions
-//////////////////////////////
-
-// testStartRepoHelper runs git init and adds the gitdo folder, which is otherwise done in the Setup() func
-func testStartRepoHelper(t *testing.T) {
-	t.Helper()
-	t.Log("Started repo")
-	cmd := exec.Command("git", "init")
-	if err := cmd.Run(); err != nil {
-		t.Fatal("could not create repo")
-	}
-	if err := os.MkdirAll(".git/gitdo/plugins/Test", os.ModePerm); err != nil {
-		t.Fatal("could not create gitdo folder")
-	}
-}
-
-func testCommitHelper(t *testing.T) {
-	cmd := exec.Command("git", "commit", "-am", "new file")
-	err := cmd.Run()
+	err = MarkSourceLines(task)
 	if err != nil {
-		t.Fatalf("error commiting file: %v", err)
+		t.Errorf("Failed to run mark lines: %v", err)
 	}
-	t.Log("Committed staged files")
-}
 
-// testAddToGitHelper runs 'git add $fileName'
-func testAddToGitHelper(t *testing.T, fileName string) {
-	t.Helper()
-	cmd := exec.Command("git", "add", fileName)
-	err := cmd.Run()
+	result, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		t.Fatalf("could not 'git add %s'", fileName)
+		t.Errorf("could not read newly marked file: %v", err)
 	}
-	t.Logf("Added file: %s", fileName)
-}
-
-// testMockFileHelper creates a file that has a TODO comment.
-func testMockFileHelper(t *testing.T) string {
-	t.Helper()
-	fileName := "main.go"
-	err := ioutil.WriteFile(fileName, []byte(mockFileContent), os.ModePerm)
-	if err != nil {
-		t.Fatal("could not create mock file")
+	if string(result) != string(newFile) {
+		t.Errorf("Expected: \n%v\n, Got: \n%v\n", newFile, result)
 	}
-	testAddToGitHelper(t, fileName)
-	return fileName
-}
-
-func testTaskFileCorrectHelper(t *testing.T) *Tasks {
-	tasks, err := getTasksFile()
-	if err != nil {
-		t.Errorf("Could not load tasks file to check: %v", err)
-	}
-	return tasks
-}
-
-func testDeleteTaskCommentHelper(t *testing.T, fileName string) {
-	fileCont, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		t.Errorf("Could not get mock file: %v", err)
-	}
-	newCont := strings.Replace(string(fileCont), "// TODO: Test <1234>\n", "", 1)
-
-	err = ioutil.WriteFile(fileName, []byte(newCont), os.ModePerm)
-	if err != nil {
-		t.Errorf("Could not write new file with todo removed: %v", err)
-	}
-	testAddToGitHelper(t, fileName)
 }
