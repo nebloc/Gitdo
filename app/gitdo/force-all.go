a90edafb36a9f9bee212af64708fc883eb86fa2f
Ben Coleman <benjamin.coleman@me.com>
2018-04-25 14:22:38 +0100

    better concurrency model for force-all

--- app/gitdo/force-all.go
+++ app/gitdo/force-all.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"github.com/nebloc/gitdo/app/utils"
-	"github.com/urfave/cli"
-	"io/ioutil"
-	"strings"
-	"github.com/nebloc/gitdo/app/versioncontrol"
+	"context"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 	"sync"
+
+	"github.com/nebloc/gitdo/app/utils"
+	"github.com/nebloc/gitdo/app/versioncontrol"
+	"github.com/urfave/cli"
+)
+
+type key int
+
+const (
+	keyHash key = iota
+	keyBranch
+
+	numberOfFileCrawlers = 5
 )
 
 var (
@@ -16,27 +27,43 @@ var (
 	pluginFailure = false
 )
 
-func ForceAll(ctx *cli.Context) error {
+func canForceAll() bool {
 	clean := config.vc.CheckClean()
 	if !clean {
 		utils.Warnf("Please start with a clean repository directory.")
-		return nil
+		return false
 	}
 
 	confirmed := ConfirmWithUser("If a lot of tasks are found you may hit the rate limit of your task manager\nAre you sure you want to run this?")
 	if !confirmed {
+		return false
+	}
+	return true
+}
+func ForceAll(cliCon *cli.Context) error {
+	if !canForceAll() {
 		return nil
 	}
+
+	filesToCheck, err := config.vc.GetTrackedFiles()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	hash, err := config.vc.GetHash()
 	if err != nil {
 		return err
 	}
+	ctx = context.WithValue(ctx, keyHash, hash)
 
 	branch, err := config.vc.GetBranch()
 	if err != nil {
 		return err
 	}
+	ctx = context.WithValue(ctx, keyBranch, branch)
 
 	utils.Highlightf("switching to new branch to make changes on - %s", versioncontrol.NewBranchName)
 	config.vc.CreateBranch()
@@ -45,28 +72,33 @@ func ForceAll(ctx *cli.Context) error {
 		return err
 	}
 
-	filesToCheck, err := config.vc.GetTrackedFiles()
-	if err != nil {
-		return err
-	}
-
+	// Start sending files
+	filec := fileSender(ctx, filesToCheck)
 	taskc := make(chan Task)
+	errorc := make(chan error)
 	donec := make(chan struct{})
-	tasks := make(map[string]Task)
 
-	for _, file := range filesToCheck {
-		go TagFile(file, hash, branch, taskc, donec)
+	for x := 0; x < numberOfFileCrawlers; x++ {
+		go crawlFiles(ctx, filec, taskc, errorc, donec)
 	}
+
+	tasks := make(map[string]Task)
+	errors := []error{}
 
 	finished := 0
-	for finished < len(filesToCheck) {
+	for finished < numberOfFileCrawlers {
 		select {
 		case task := <-taskc:
 			tasks[task.id] = task
+		case err := <-errorc:
+			cancel()
+			errors = append(errors, err)
+			fmt.Println(err)
 		case <-donec:
 			finished++
 		}
 	}
+
 	if len(tasks) == 0 {
 		utils.Highlight("No tasks found.")
 		return nil
@@ -86,20 +118,36 @@ func ForceAll(ctx *cli.Context) error {
 	if err != nil {
 		utils.Warnf("Could not commit changes: %v", err)
 	}
-	utils.Highlight("Please run any unit tests to ensure the code's working\nCheck the diff with 'git diff HEAD~1', before merging")
+	utils.Highlight("Please run any unit tests to ensure the code's working\nCheck the diff with 'git diff HEAD~1 -U0', before merging")
 	return nil
 }
-func TagFile(fileName string, hash string, branch string, taskc chan<- Task, donec chan<- struct{}) {
-	if fileName == "" {
-		donec <- struct{}{}
-		return
-	}
 
-	cont, err := ioutil.ReadFile(fileName)
+func crawlFiles(ctx context.Context, filec <-chan string, taskc chan<- Task, errorc chan<- error, done chan<- struct{}) {
+	for {
+		select {
+		case <-ctx.Done():
+			done <- struct{}{}
+			return
+		case filename, open := <-filec:
+			if !open {
+				done <- struct{}{}
+				return
+			}
+
+			if err := processFile(ctx, filename, taskc); err != nil {
+				errorc <- err
+				done <- struct{}{}
+				return
+			}
+		default:
+		}
+	}
+}
+
+func processFile(ctx context.Context, filename string, taskc chan<- Task) error {
+	cont, err := ioutil.ReadFile(filename)
 	if err != nil {
-		utils.Dangerf("Could not read: %s to tag", fileName)
-		donec <- struct{}{}
-		return
+		return fmt.Errorf("could not read file %s: %v", filename, err)
 	}
 
 	sep := "\n"
@@ -108,7 +156,9 @@ func TagFile(fileName string, hash string, branch string, taskc chan<- Task, don
 		sep = "\r\n"
 	}
 
+	var latestError error
 	changed := false
+
 	for ind, line := range lines {
 		line = utils.StripNewlineString(line)
 		taskname, isTask := CheckRegex(looseTODOReg, line)
@@ -117,33 +167,31 @@ func TagFile(fileName string, hash string, branch string, taskc chan<- Task, don
 			if _, isTagged := CheckRegex(taggedReg, line); isTagged {
 				continue
 			}
-			utils.Highlightf("Found: %s#L%d - %s", fileName, ind+1, taskname)
+			utils.Highlightf("Found: %s#L%d - %s", filename, ind+1, taskname)
 
 			// Create Task
 			t := Task{
 				id:       "",
-				FileName: fileName,
+				FileName: filename,
 				TaskName: taskname,
 				FileLine: ind + 1,
 				Author:   config.Author,
-				Hash:     hash,
-				Branch:   branch,
+				Hash:     ctx.Value(keyHash).(string),
+				Branch:   ctx.Value(keyBranch).(string),
+			}
+			select {
+			case <-ctx.Done():
+				fmt.Println("Context canceled")
+				break
+			default:
 			}
 
-			mu.Lock()
-			if pluginFailure {
-				mu.Unlock()
-				break
-			}
 			// Get ID for task
 			resp, err := RunPlugin(CREATE, t)
 			if err != nil {
-				pluginFailure = true
-				utils.Dangerf("couldn't get ID for task in plugin: %s, %v", resp, err)
-				mu.Unlock()
+				latestError = fmt.Errorf("error creating task: %v", err)
 				break
 			}
-			mu.Unlock()
 
 			t.id = resp
 			taskc <- t
@@ -159,12 +207,120 @@ func TagFile(fileName string, hash string, branch string, taskc chan<- Task, don
 	}
 
 	if changed {
-		err := ioutil.WriteFile(fileName, []byte(strings.Join(lines, sep)), os.ModePerm)
+		err := ioutil.WriteFile(filename, []byte(strings.Join(lines, sep)), os.ModePerm)
 		if err != nil {
-			utils.Dangerf("Could not tag %s", fileName)
+			utils.Dangerf("Could not tag %s", filename)
 		}
 	}
 
-	donec <- struct{}{}
-	return
+	return latestError
 }
+
+func fileSender(ctx context.Context, files []string) chan string {
+	filec := make(chan string, 5)
+	go func() {
+		fileCount := 0
+		for _, file := range files {
+			if strings.TrimSpace(file) == "" {
+				continue
+			}
+			select {
+			case <-ctx.Done():
+				fmt.Printf("context done thrown")
+				close(filec)
+				return
+			default:
+			}
+			filec <- file
+			fileCount++
+		}
+		fmt.Printf("Files Checked: %d\n", fileCount)
+		close(filec)
+		ctx.Done()
+	}()
+	return filec
+}
+
+/**
+func TagFile(filec chan string, hash string, branch string, taskc chan<- Task, donec chan<- struct{}) {
+	for {
+		fileName := <-filec
+		if fileName == "" {
+			donec <- struct{}{}
+			return
+		}
+
+		cont, err := ioutil.ReadFile(fileName)
+		if err != nil {
+			utils.Dangerf("Could not read: %s to tag", fileName)
+			donec <- struct{}{}
+			return
+		}
+
+		sep := "\n"
+		lines := strings.Split(string(cont), sep)
+		if isCRLF(lines[0]) {
+			sep = "\r\n"
+		}
+
+		changed := false
+		for ind, line := range lines {
+			line = utils.StripNewlineString(line)
+			taskname, isTask := CheckRegex(looseTODOReg, line)
+			if isTask {
+				// Ignore tagged tasks
+				if _, isTagged := CheckRegex(taggedReg, line); isTagged {
+					continue
+				}
+				utils.Highlightf("Found: %s#L%d - %s", fileName, ind+1, taskname)
+
+				// Create Task
+				t := Task{
+					id:       "",
+					FileName: fileName,
+					TaskName: taskname,
+					FileLine: ind + 1,
+					Author:   config.Author,
+					Hash:     hash,
+					Branch:   branch,
+				}
+
+				mu.Lock()
+				if pluginFailure {
+					mu.Unlock()
+					break
+				}
+				// Get ID for task
+				resp, err := RunPlugin(CREATE, t)
+				if err != nil {
+					pluginFailure = true
+					utils.Dangerf("couldn't get ID for task in plugin: %s, %v", resp, err)
+					mu.Unlock()
+					break
+				}
+				mu.Unlock()
+
+				t.id = resp
+				taskc <- t
+
+				fStr := "%s <%s>"
+				if sep == "\r\n" {
+					fStr = "%s <%s>\r"
+				}
+
+				lines[ind] = fmt.Sprintf(fStr, line, t.id)
+				changed = true
+			}
+		}
+
+		if changed {
+			err := ioutil.WriteFile(fileName, []byte(strings.Join(lines, sep)), os.ModePerm)
+			if err != nil {
+				utils.Dangerf("Could not tag %s", fileName)
+			}
+		}
+	}
+	donec <- struct{}{}
+}
+
+*/
